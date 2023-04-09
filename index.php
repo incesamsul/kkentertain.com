c6148202a4ae342d447453f2af30d6943d8476c7
incesamsul <incesamsul89@gmail.com>
2023-04-09 11:44:40 +0800

    initial init

---
+++ index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KK Entertain</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+    <?php
+    $flexCenter = 'd-flex align-items-center justify-content-center';
+    ?>
+
+
+    <nav class="navbar navbar-expand-sm navbar-light p-4 fixed-top">
+        <div class="container">
+            <a class="navbar-brand " href="#"><strong>KK Entertain</strong></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+                <ul class="navbar-nav ">
+                    <li class="nav-item mx-3">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">Lighting</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="#">Sound</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="hero" class="full-height <?= $flexCenter ?> flex-column">
+        <div class="container ">
+            <div class="row  <?= $flexCenter ?>">
+                <div class="col-sm-6 pe-5">
+                    <h1>KK Entertain</h1>
+                    <p>Event Organizer Makassaar, Sewa Sound System Makassar, Sewa Lighting Makassar</p>
+                    <button class="btn btn-outline-warning">Hubungi kami</button>
+                </div>
+                <div class="col-sm-6">
+                    <img src="./assets/img/sounds.jpg" alt="gambar sound system" width="500">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="sound" class="py-5 full-height secondary">
+        <div class="container">
+            <div class="row ">
+                <div class="col-sm-6 offset-sm-3 text-center">
+                    <h2 class="mb-3"><strong>Event Organizer Makassar</strong></h2>
+                    <p class="lh-base"><small>tempat yang tepat untuk mengorganisir acara impian Anda. Dengan lebih dari
+                            10 tahun
+                            pengalaman dalam industri ini
+                        </small></p>
+                    <button class="btn btn-outline-warning">Konsultasi</button>
+                </div>
+            </div>
+            <div class="row mt-5 <?= $flexCenter ?>">
+                <div class="col-sm-6 px-5">
+                    <h5 class="lh-base text-capitalize"><strong>Sound system berkualitas tinggi dari merek terkemuka dunia.</strong></h5>
+                    <p class="text-secondary lh-base"><small>Produk-produk kami berasal dari merek-merek terkemuka di
+                            industri
+                            lampu panggung dunia, sehingga Anda dapat yakin bahwa Anda mendapatkan produk berkualitas
+                            tinggi dan dapat diandalkan.</small></p>
+                    <div class="button-warpper">
+                        <button class="btn btn-outline-secondary me-2">YAMAHA DSR 118 </button>
+                        <button class="btn btn-outline-secondary me-2">DSR 115 </button>
+                        <button class="btn btn-outline-secondary me-2">MGP 24X</button>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <a href="https://www.freepik.com/free-vector/loud-speakers-isolated-template_8314391.htm#query=3d%20ilustration%20sound%20system&position=32&from_view=search&track=ais">
+                        <img src="./assets/img/1021.png" alt="sound 3d ilustration" width="400">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="lighting" class="py-5 full-height  <?= $flexCenter ?>">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-sm-6 px-5">
+                    <h5 class="lh-base text-capitalize"><strong>Lampu panggung berkualitas tinggi dari merek terkemuka dunia.</strong></h5>
+                    <p class="text-secondary lh-base"><small>Kami memiliki berbagai jenis lampu panggung, termasuk lampu beam moving head, PARLED 54X3 RGBW, lampu strobo, FOLLOWSPOT, PAR 64, Fresnel, HPIT, dan berbagai jenis lampu efek panggung yang dapat digunakan untuk segala kebutuhan event, pentas konser, gathering, seminar, festival, wedding, teater, dan hajatan
+                        </small></p>
+                    <button class="btn btn-outline-secondary me-2">Beam Moving Head </button>
+                    <button class="btn btn-outline-secondary me-2">Par 64 </button>
+                    <button class="btn btn-outline-secondary me-2">Fresnel</button>
+                </div>
+                <div class="col-sm-6 <?= $flexCenter ?>">
+                    <img src="./assets/img/light.png" alt="sound 3d ilustration" width="300">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="sound" class="py-5 full-height secondary <?= $flexCenter ?>">
+        <div class="container">
+            <div class="row mt-5 <?= $flexCenter ?>">
+                <div class="col-sm-6 px-5">
+                    <h5 class="lh-base text-capitalize"><strong>Teknisi andal dan profesional.</strong></h5>
+                    <p class="text-secondary lh-base"><small>Kami juga memiliki tim teknisi berpengalaman dan profesional yang siap membantu Anda dalam perencanaan, instalasi, dan pengaturan lampu acara Anda. kami mengutamakan kepuasan pelanggan dan selalu berusaha untuk memberikan solusi pencahayaan terbaik.</small></p>
+
+                </div>
+                <div class="col-sm-6 <?= $flexCenter ?>">
+                    <a href="https://www.freepik.com/free-psd/3d-illustration-delivery-man-character-handling-boxes_29014099.htm#query=3d%20technician&position=27&from_view=search&track=ais">
+                        <img src="./assets/img/teknisi.png" alt="sound 3d ilustration" width="600">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row full-height <?= $flexCenter ?>  ">
+                <div class="col-sm-6 offset-sm-2 text-center">
+                    <h1 class="lh-base"><strong>Segera hubungi kami untuk informasi lebih lanjut tentang produk dan layanan.</strong></h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="p-5 mt-5 bg-second">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h4>KK Entertain</h4>
+                    <p class="text-secondary"><small>
+                            KK Entertain adalah perusahaan event organizer yang berbasis di makassar, Indonesia. Perusahaan ini berfokus pada penyelenggaraan acara-acara hiburan seperti konser musik, pertunjukan seni, festival, dan acara perusahaan. </small></p>
+                </div>
+                <div class="col-sm-3">
+                    <ul>
+                        <li><strong>About</strong></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Lighting</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Event organizer</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">sound system</a>
+                            </small></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <ul>
+                        <li><strong>About</strong></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Lighting</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Event organizer</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">sound system</a>
+                            </small></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <ul>
+                        <li><strong>About</strong></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Lighting</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">Event organizer</a>
+                            </small></li>
+                        <li class="text-secondary"><small>
+                                <a href="">sound system</a>
+                            </small></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
