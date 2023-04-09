6129a4bbd151ea5556ec7ca5a3ff9f1064be8a30
incesamsul <incesamsul89@gmail.com>
2023-04-09 11:49:21 +0800

    fix meta tag

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <html lang="en">
 
 <head>
+    <title>KK Entertain | Sewa sound makassar | Sewa lighting makassar | Profile</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>KK Entertain</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name='robots' content='max-image-preview:large' />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="sewa lighting dan sound makassar" />
+    <meta property="og:description" content="rental lighting dan sound makassar" />
+    <meta property="og:url" content="https://kkentertain.com" />
+    <meta property="og:site_name" content="botting id" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
